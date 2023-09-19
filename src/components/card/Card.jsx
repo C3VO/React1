e8a6ba0c53407fcd546f002f56e30320779a57a7
Vlas <pensioner555@gmail.com>
2023-09-19 08:10:13 +0300

    first react app

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,28 @@
+import styles from './Card.module.css';
+import cat01Img from './../../img/categories/cat-01.jpg';
+import arrowImg from './../../img/icons/arrow.svg';
+const Card = (props) => {
+    console.log(props);
+    const { title, img } = props
+    return (
+        <div className={styles.card}>
+            <a href="#!" className={styles.card__link}></a>
+            <img
+                className={styles.card__img}
+                src={img}
+                alt="Category..." />
+            <div className={styles.card__body}>
+                <div className={styles.card__text}>
+                    <div className={styles.card__title}>{title}</div>
+                    <div className={styles.card__muted}>Explore now!</div>
+                </div>
+                <div className={styles.card__icon}>
+                    <img src={arrowImg} alt="ArrowImg" />
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Card;
